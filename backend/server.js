88c421dfff27ec89d5524fe97f672c424fd57fe7
JairAlberto2000 <jair.alberto1905@gmail.com>
2025-06-03 08:20:51 -0600

    Se modifican los codigos QR, se agrega el apartado de admin en la página web y su respecyiva conexión con la BD

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const authRoutes = require('./routes/auth.routes');
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3010;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
